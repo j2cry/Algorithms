fb28aa8e2da9634ebb457daf4903f11848f56683
avagadro <fragarie@yandex.com>
2021-01-27 23:59:10 +0300

    Lesson 1

--- lesson_1/task_5.py
+++ lesson_1/task_5.py
@@ -1,9 +1,5 @@
 # Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
 
-from string import ascii_lowercase
-
-char = input('Enter char: ')
-
-pos = ascii_lowercase.find(char.lower()) + 1
-
-print(f'Char "{char}" position: {pos}')
+pos = int(input('Enter char number: '))
+char = chr(ord('a') + pos - 1)
+print(f'Char "{char}"')
